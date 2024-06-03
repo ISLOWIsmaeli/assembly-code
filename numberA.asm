568b4968448104efde3414ca8707c5f8d6d88a76
Ismael126895 <Lenojore67@gmail.com>
2024-06-03 22:31:29 +0300

    modified and understood exercise A

--- numberA.asm
+++ numberA.asm
@@ -7,14 +7,15 @@ MOV AL, [SI]   ; Load the value at [SI] into AL
 
 ; Load the second number from memory address 1001H into BL
 MOV SI, 1001H  ; Set SI to point to offset 1001H  
-MOV [SI],010H
+MOV [SI],010H  ; store value in SI memory
 MOV BL, [SI]   ; Load the value at [SI] into BL
 
 ; Add the two numbers
-ADD AL, BL     ; Add the value in BL to AL
+ADD AL, BL     ; Add the value in BL to AL and store in AL
 
 ; Load the third number from memory address 1002H into BL
 MOV SI, 1002H  ; Set SI to point to offset 1002H
+MOV [SI],022H  ; store a number in the 1002H offset pointer
 MOV BL, [SI]   ; Load the value at [SI] into BL
 
 ; Perform an OR operation between the result in AL and the value in BL
