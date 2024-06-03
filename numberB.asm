f9673ca0d03d9956fe726f967a1e0f6e52eee5ca
Ismael126895 <Lenojore67@gmail.com>
2024-06-03 22:48:58 +0300

    modified number B

--- numberB.asm
+++ numberB.asm
@@ -12,12 +12,12 @@ ADD_LOOP:
     ADD SI, 2      ; Move to the next 16-bit number
     LOOP ADD_LOOP  ; Repeat the addition for all 8 numbers
 
-    ; Perform an AND operation with 8888H
-    AND AX, 8888H  ; Perform AND operation with the result
+; Perform an AND operation with 8888H
+AND AX, 8888H  ; Perform AND operation with the result
 
-    ; Store the result in memory addresses 1400H and 1401H
-    MOV DI, 1400H  ; DI points to the start of the result
-    MOV [DI], AX   ; Store the low byte of the result at 1400H
-    MOV [DI + 2], AH ; Store the high byte of the result at 1401H
+; Store the result in memory addresses 1400H and 1401H
+MOV DI, 1400H  ; DI points to the start of the result
+MOV [DI], AX   ; Store the low byte of the result at 1400H
+MOV [DI + 2], AH ; Store the high byte of the result at 1401H
 
-    RET            ; Return from the program
+RET            ; Return from the program
